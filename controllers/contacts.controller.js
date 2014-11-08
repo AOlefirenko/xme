183286e4010248ba883792e9a06e40f569bcadb2
Andrey Olefirenko <ole.olefirenko>
2014-11-08 20:39:11 +0300

    fixed

--- controllers/contacts.controller.js
+++ controllers/contacts.controller.js
@@ -10,8 +10,8 @@ exports.get = function(req, res,next) {
     console.log(id);
     req.db.collection('users').findOne({_id:new ObjectId(id)},function(err, doc){
         if(err) return next(errors.InternalServerError(err.message));
-        console.log('i am',doc);
-        req.db.collection('users').find({nick:{$in:doc.contacts}}).toArray(function(docs){
+        console.log({nick:{$in:doc.contacts}});
+        req.db.collection('users').find({nick:{$in:doc.contacts}}).toArray(function(err,docs){
             var contacts = docs.map(function(d){
                 return {id:d.id, pic: d.pic, nick: d.nick, firstName: d.firstName, lastName: d.lastName};
             });
