b63e22efc3c521cb570cc9263c15e30d7a5a5f1e
Andrey Olefirenko <ole.olefirenko>
2014-11-08 20:52:45 +0300

    fix

--- modules/syncFbFriends.js
+++ modules/syncFbFriends.js
@@ -21,10 +21,13 @@ module.exports = function(db, id,token,contacts){
         },
         function(result,callback){
             var ids = result.data.map(function(i){return i.id});
+            console.log("fb friends",ids);
             db.collection('users').find({fbId:{$in:ids}}).toArray(callback);
         },
         function(result, callback){
             var ids = result.map(function(i){return i.nick});
+            console.log("nicks",ids);
+            console.log("my contacts",contacts);
             var changed = false;
             ids.forEach(function(i){
                 var res = _.find(contacts,function(c){return c === i;}) ;
