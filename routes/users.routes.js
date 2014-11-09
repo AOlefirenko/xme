5380f624235105f43e5975fc1eb7c4243e06fda9
Andrey Olefirenko <ole.olefirenko>
2014-11-09 20:40:21 +0300

    fixed

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,8 +1,8 @@
 var controller =  require('../controllers/users.controller');
 
 module.exports  = function(app){
-    app.get('/api/users/me',controller.getMe);
-    app.patch('/api/users/me',controller.patch);
+    app.route('/api/users/me').get(controller.getMe)
+        .patch(controller.patch);
 
     app.get('/users/checker',controller.checkUser);
 }
