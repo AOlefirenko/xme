3519667695a32b770ce5f4f1abaf9a4eb81a606b
Andrey Olefirenko <ole.olefirenko@gmail.com>
2014-11-09 13:11:08 +0300

    fix

--- realtime.js
+++ realtime.js
@@ -1,18 +1,15 @@
-var WebSocketServer = require('ws').Server;
+var http = require('http');
+var server = http.createServer();
+server.listen(8001);
 
-    var http = require('http');
-    var server = http.createServer();
-    server.listen(8001);
-    var wss = new WebSocketServer({server: server});
-    wss.on('connection', function(ws) {
-        console.log(ws);
-        ws.on('message', function(message,a,b) {
-            ws.send("reverse!");
-            ws.send(message);
-        });
-        ws.send("Hello!!!!!");
+var WebSocketServer = require('ws').Server,
+    wss = new WebSocketServer({server: server});
 
+wss.on('connection', function(ws) {
+    ws.on('message', function(message,a,b) {
+        ws.send("reverse!");
+        ws.send(message);
     });
+    ws.send("Hello!!!!!");
 
-
-
+});
