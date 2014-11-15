0ea6ff71324324fd22069f2c5723b4b17190e842
Andrey Olefirenko <ole.olefirenko@gmail.com>
2014-11-15 22:27:06 +0300

    fixed

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -20,19 +20,24 @@ exports.login = function(req, res,next) {
 }
 
 exports.logout = function(req, res,next) {
-    var data = jwt.decode(req.query.accessToken);
+    try {
+        var data = jwt.decode(req.query.accessToken, 'xxx');
+        if(data.provider=='facebook')
+        {
+            var id = new ObjectId(data.id);
+            req.db.collection('users').findOne({_id: id}, function(err,doc){
+                var url = "https://www.facebook.com/logout.php?next=http%3A%2F%2Fxme.cloudapp.net%2F&access_token="+doc.providerData.accessToken;
+                res.redirect(url);
+            });
+        }
+        else{
+            res.send();
+        }
+    }
+    catch(e){
+        res.status(400).send("invalid token");
+    }
 
-    if(data.provider=='facebook')
-    {
-        var id = new ObjectId(data.id);
-        req.db.collection('users').findOne({_id: id}, function(err,doc){
-            var url = "https://www.facebook.com/logout.php?next=http%3A%2F%2Fxme.cloudapp.net%2F&access_token="+doc.providerData.accessToken;
-            res.redirect(url);
-        });
-    }
-    else{
-        res.send();
-    }
 }
 
 exports.register = function(req, res,next) {
