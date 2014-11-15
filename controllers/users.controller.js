0e6d29859e6842d78e4f5ca8378cbea71e71f33c
Andrey Olefirenko <ole.olefirenko>
2014-11-15 12:42:42 +0300

    fix

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -19,10 +19,8 @@ exports.checkUser = function(req, res,next) {
 }
 
 exports.patch = function(req, res,next) {
-    console.log("patch user");
     var query = {_id:new ObjectId(req.user.id)};
     var update = {$set:req.body};
-    console.log('PATCH',query,update);
     req.db.collection('users').update(query,update,function(err,doc){
         if(err) {
             next(errors.InternalServerError(err.message));
