46a6666345f5a2ccffa9edbb6f140025385481c6
Andrey Olefirenko <ole.olefirenko@gmail.com>
2014-11-19 16:35:56 +0300

    Update contacts.controller.js

--- controllers/contacts.controller.js
+++ controllers/contacts.controller.js
@@ -24,7 +24,7 @@ exports.get = function(req, res,next) {
 
 exports.addContact = function(req,res,next){
     var id = new ObjectId(req.user.id);
-    req.db.collection('users').update({_id:id},{$push:{contacts:req.params.username}},function(err,result){
+    req.db.collection('users').update({_id:id},{$addToSet:{contacts:req.params.username}},function(err,result){
         if(err) return next(errors.InternalServerError(err.message));
         res.status(204).send()
     });
