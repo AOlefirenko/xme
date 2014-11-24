fadc8187bef00c1c9bdcbdcbf16eb81d71b76cac
Andrey Olefirenko <ole.olefirenko>
2014-11-24 19:14:15 +0300

    session deletion

--- controllers/sessions.controller.js
+++ controllers/sessions.controller.js
@@ -7,8 +7,19 @@ exports.createSession = function(req, res) {
     var doc = {userId:id};
     req.db.collection('sessions').insert(doc,function(err, result){
         if(err) return next(errors.InternalServerError(err.message));
-        req.db.collection('users').update({_id:doc},{$addToSet:{sessions:doc._id}},function(err, result){
+        req.db.collection('users').update({_id:id},{$addToSet:{sessions:doc._id}},function(err, result){
             res.send({id:doc._id});
+        });
+    });
+}
+exports.deleteSession = function(req, res) {
+    var id = new ObjectId(req.user.id);
+    var sessionId = new ObjectId(req.params.id);
+    var doc = {userId:id,_id: sessionId};
+    req.db.collection('sessions').remove(doc,function(err, result){
+        if(err) return next(errors.InternalServerError(err.message));
+        req.db.collection('users').update({_id:doc},{$pull:{sessions:sessionId}},function(err, result){
+            res.send();
         });
     });
 }
--- routes/sessions.routes.js
+++ routes/sessions.routes.js
@@ -2,4 +2,5 @@ var controller =  require('../controllers/sessions.controller');
 
 module.exports  = function(app){
     app.route('/api/sessions/').post(controller.createSession);
+    app.route('/api/sessions/:id').delete(controller.deleteSession);
 }
