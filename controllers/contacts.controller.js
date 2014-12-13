e839f658470e212d016998d276396984ba8d034e
Andrey Olefirenko <ole.olefirenko>
2014-12-13 13:25:25 +0300

    fix

--- controllers/contacts.controller.js
+++ controllers/contacts.controller.js
@@ -1,6 +1,7 @@
 var ObjectId = require("mongodb").ObjectID,
     https = require('https'),
     async = require('async'),
+	_ = require('lodash'),
     errors = require('http-custom-errors');
 
 
@@ -10,13 +11,14 @@ exports.get = function(req, res,next) {
     console.log(id);
     req.db.collection('users').findOne({_id:new ObjectId(id)},function(err, doc){
         if(err) return next(errors.InternalServerError(err.message));
-        console.log(doc);
-        console.log({nick:{$in:doc.contacts}});
+        
         req.db.collection('users').find({nick:{$in:doc.contacts || []}}).toArray(function(err,docs){
             var contacts = docs.map(function(d){
                 return {id:d.id, pic: d.pic, nick: d.nick, firstName: d.firstName, lastName: d.lastName,type: d.type};
             });
-            console.log('contacts',contacts);
+			if(doc.gisSearches) _.each(doc.gisSearches,function(val){
+					contact.push({nick:val,pic:'http://xme.cloudapp.net/img/gis-placeholder.jpg'})
+				});
             res.send(contacts);
         })
     });
@@ -24,8 +26,16 @@ exports.get = function(req, res,next) {
 
 exports.addContact = function(req,res,next){
     var id = new ObjectId(req.user.id);
+	if(req.query.type=='2gis'){
+		req.db.collection('users').update({_id:id},{$addToSet:{gisSearches:req.params.username}},function(err,result){
+			if(err) return next(errors.InternalServerError(err.message));
+			res.status(204).send()
+		});
+		return;
+	}
     req.db.collection('users').update({_id:id},{$addToSet:{contacts:req.params.username}},function(err,result){
         if(err) return next(errors.InternalServerError(err.message));
         res.status(204).send()
     });
 }
+
